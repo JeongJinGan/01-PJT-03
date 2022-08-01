0ec44ee86da766f1922768589f511fef67b38a32
JeongJinGan <rksaudwls123@naver.com>
2022-08-01 23:17:46 +0900

    update secretdoor

--- 3회차/간정진/6_암호문1.py
+++ 3회차/간정진/6_암호문1.py
@@ -1,24 +1,44 @@
+# 명령어 = 명령어리스트[0]
+# if 명령어 == "I":
+#   x = 명령어리스트[0 + 1]
+#   y = 명령어리스트[0 + 2]
+#   숫자리스트 = 명령어리스트[0 + 3 : 0 + 3 + y]
+#   for 숫자 in 숫자_리스트[::-1]:
+#       암호문.inser(삽입_인덱스, 숫자) # 0 -> 1
+#----------------------------------------------------#
 import sys
 
-sys.stdin = open("_암호문1.txt", 'r')
+sys.stdin = open("_암호문1.txt", "r")
 
-list_ = []
+T = 10
+# ctrl + d 단축키 쉼표 표시하고 단추키 쓰면 쉼표만 표시 가능
+# 원본암호문 = [449047, 855428, 425117, 532416, 358612, 929816, 313459, 311433, 472478, 589139, 568205]
 
-o_length = int(input()) # 원본 암호문의 길이
-o_secret_ = list(map(int, input().split())) # 원본 암호문
-command_num = int(input()) # 명령어의 개수
+for t in range(1, T+1):
+    origin_len = int(input())
+    origin_list = list(map(int, input().split()))
 
-for i in range(command_num): # 명령어의 개수 만큼
-    del_ = input() # I 입력 받기 (이게 안되네)
-    print(del_)
-    y = int(input()) # y값(숫자 삽입) 입력 받기
-    s = int(input()) # s값(덧붙일 숫자) 입력 받기
- 
-    for j in s:
-        command_ = list(input().split())
+    command_len = int(input())
+    command_list = input().split()
+
+    # i의 초기화
+    i = 0
+
+    # while 문 
+    while i < len(command_list):
+        command = command_list[i]
+        if command == "I":
+            # x, y 숫자리스트 s 구해야한다.
+            x = int(command_list[i+1])
+            y = int(command_list[i+2])
+            number_list = command_list[i + 3 : i + 3 + y]
+
+            # insert 메서드를 써서 x의 위치에 숫자들을 삽입한다.
+            # 역순으로 삽입한다.
+            for number in number_list[::-1]:
+                origin_list.insert(x, int(number))
+        
+        # 0 + 1 -> 1
+        i = i + 1
     
-    for k in j-s:
-        list_.append(command_[k])
-
-for f in range(10):
-    print(command_.get(f))
+    print(f'#{t}' ,*origin_list[:10])
